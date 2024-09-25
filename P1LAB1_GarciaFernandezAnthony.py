e7b0843ce2f77123912daa99f355042fa9b50f3a
garciafa8145 <garciafa8145@student.faytechcc.edu>
2024-09-25 10:30:34 -0400

    Delete P1LAB1_GarciaFernandezAnthony.py

--- P1LAB1_GarciaFernandezAnthony.py
+++
@@ -1,8 +0,0 @@
-# Anthony Garcia-Fernandez
-# 2024-09-25
-# P1LAB1 - Welcome Program
-# This program prompts the user to enter their first and last name and then displays a welcome message.
-
-first_name = input("Please enter your first name: ")
-last_name = input("Please enter your last name: ")
-print(f"Welcome, {first_name} {last_name}, to CTI-110!")
